f74eec41f83e9a39e0afe5e46e9809a51bdecc74
Daniel Bas <daniel@Daniels-MacBook-Pro.local>
2024-02-09 11:44:06 +0100

    adding one project

--- next-app/lib/data.ts
+++ next-app/lib/data.ts
@@ -24,6 +24,10 @@ export const links = [
     hash: "#experience",
   },
   {
+    name: "Projects",
+    hash: "#projects",
+  },
+  {
     name: "Contact",
     hash: "#contact",
   },
@@ -74,25 +78,21 @@ export const experiencesData = [
 
 export const projectsData = [
   {
-    title: "CorpComment",
+    title: "Cloud Portfolio",
     description:
-      "I worked as a full-stack developer on this startup project for 2 years. Users can give public feedback to companies.",
-    tags: ["React", "Next.js", "MongoDB", "Tailwind", "Prisma"],
+      "I created a portfolio website using multiple AWS services as well as an IaC approach with Terraform. ",
+    tags: [
+      "Next.js",
+      "CloudFront",
+      "Lambda",
+      "S3",
+      "Api Gateway",
+      "Route53",
+      "Certificate Manager",
+
+      "Terraform",
+    ],
     imageUrl: corpcommentImg,
-  },
-  {
-    title: "rmtDev",
-    description:
-      "Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.",
-    tags: ["React", "TypeScript", "Next.js", "Tailwind", "Redux"],
-    imageUrl: rmtdevImg,
-  },
-  {
-    title: "Word Analytics",
-    description:
-      "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
-    tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
-    imageUrl: wordanalyticsImg,
   },
 ] as const;
 
@@ -114,5 +114,4 @@ export const skillsData = [
   "JavaScript",
   "TypeScript",
   "React",
-  "Next.js",
 ] as const;
